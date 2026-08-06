cfb0f87781d61e7ab51ddac471bf0f1abd2efb01
ASUS <florence_saludes@dlsu.edu.ph>
2026-08-06 23:07:30 +0800

    remove graduation photo from contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -57,21 +57,6 @@ export default function ContactPage() {
           </li>
         </ul>
       </Reveal>
-
-      <Reveal direction="right">
-        <div>
-          <div className="section-label mb-6">
-            <span>Graduation photo</span>
-          </div>
-          <div className="flex items-center justify-center">
-            <img
-              src="/image.png"
-              alt="Graduation photo"
-              className="rounded-lg border border-line max-w-full h-auto"
-            />
-          </div>
-        </div>
-      </Reveal>
     </div>
   );
 }
