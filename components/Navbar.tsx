5904eb6f864051782c8d01301576ddf389be4128
ASUS <florence_saludes@dlsu.edu.ph>
2026-08-07 14:09:57 +0800

    Update portfolio

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
-import { Menu, X, Command, Home } from "lucide-react";
+import { Menu, X, Command } from "lucide-react";
 import { ThemeToggle } from "./ThemeToggle";
 import { cn } from "@/lib/utils";
 
@@ -34,7 +34,20 @@ export function Navbar({ onOpenPalette }: { onOpenPalette: () => void }) {
           }}
           className="text-ink"
         >
-          <Home size={16} />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+            <polyline points="9 22 9 12 15 12 15 22" />
+          </svg>
         </Link>
 
         <div className="hidden lg:flex items-center gap-6">
